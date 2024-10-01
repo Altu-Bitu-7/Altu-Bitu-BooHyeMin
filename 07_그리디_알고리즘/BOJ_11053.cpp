7b9af6d90999f72f9130bc12c31118be5278da73
hyeminboo <hyemin110415@gmail.com>
2024-10-01 18:59:41 +0900

    [동적계획법] 10월 1일

--- 07_그리디_알고리즘/BOJ_11053.cpp
+++ 07_그리디_알고리즘/BOJ_11053.cpp
@@ -10,21 +10,21 @@ int main() {
     int n;
     cin >> n;
 
-    int a[1001], dp[1001];
+    int a[1001], dp[1001]; // dp : 각 원소까지의 가장 긴 부분수열 길이 저장
 
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
 
-    int sum = 0;
+    int sum = 0; // 최대 길이
     for (int i = 1; i <= n; i++) {
         dp[i] = 1;
         for (int j = 1; j < i; j++) {
-            if (a[i] > a[j]) {
-                dp[i] = max(dp[i], dp[j] + 1);
+            if (a[i] > a[j]) { // 이전 원소와 비교 (a[i]가 a[j]보다 크면 a[i]를 부분 수열에 포함)
+                dp[i] = max(dp[i], dp[j] + 1); // dp[i] 갱신
             }
         }
-        sum = max(sum, dp[i]);
+        sum = max(sum, dp[i]); // 최대 길이 갱신
     }
     cout << sum;
 
